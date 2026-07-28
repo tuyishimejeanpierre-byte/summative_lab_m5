6be0885a922955a298d2627f91bfbb3938a14574
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-07-28 07:39:23 +0300

    Add POST workouts endpoint

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 
 from models import db, Workout
-from schemas import WorkoutSchema
+from schemas import WorkoutSchema, WorkoutDetailSchema
 
 
 app = Flask(__name__)
@@ -24,7 +24,45 @@ def get_workouts():
         schema.dump(workouts),
         200
     )
+@app.route("/workouts/<int:id>", methods=["GET"])
+def get_workout(id):
+    workout = db.session.get(Workout, id)
 
+    if not workout:
+        return make_response(
+            {"error": "Workout not found"},
+            404
+        )
+
+    schema = WorkoutDetailSchema()
+
+    return make_response(
+        schema.dump(workout),
+        200
+    )
+@app.route("/workouts", methods=["POST"])
+def create_workout():
+    data = request.get_json()
+
+    schema = WorkoutSchema()
+
+    try:
+        workout_data = schema.load(data)
+    except Exception as error:
+        return make_response(
+            {"errors": error.messages},
+            400
+        )
+
+    workout = Workout(**workout_data)
+
+    db.session.add(workout)
+    db.session.commit()
+
+    return make_response(
+        schema.dump(workout),
+        201
+    )
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
